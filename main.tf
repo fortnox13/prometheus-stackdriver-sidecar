206292434fa1989cce64c1046b90f039be71a082
Mykyta Demchenko <nekitdem@gmail.com>
2021-08-06 11:39:39 +0000

    add creating namespace and intalation node-exporter

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ data "kubectl_path_documents" "prometheus" {
   pattern = "./manifests/2-gke-prometheus-deployment.yaml"
   vars = {
     KUBE_NAMESPACE     = "prometheus"
-    KUBE_CLUSTER       = "gke"
-    GCP_LOCATION       = "us-central1-c"
-    GCP_PROJECT        = "tttttttttttttttttttttttttttttt"
+    KUBE_CLUSTER       = "cluster-2"
+    GCP_LOCATION       = "us-west2-a"
+    GCP_PROJECT        = "test-last-one"
     DATA_DIR           = "/prometheus"
     DATA_VOLUME        = "prometheus-storage-volume"
     SIDECAR_IMAGE_TAG  = "0.8.2"
@@ -31,4 +31,18 @@ resource "kubectl_manifest" "prometheus" {
 #     kubernetes = kubernetes.gke-1
 #   }
   yaml_body = element(data.kubectl_path_documents.prometheus.documents, 1)
+}
+
+
+resource "helm_release" "node-exporter" {
+  name             = "node-exporter"
+  repository       = "prometheus-community.github.io/helm-charts"
+  chart            = "prometheus-community/prometheus-node-exporter"
+  create_namespace = false
+  namespace        = "prometheus"
+
+  values = [
+#     # The file with all values that should override the default values for the chart:
+#     "${file("manifests/pingfederate.yaml")}"
+  ]
 }
--- provider.tf
+++ provider.tf
@@ -6,14 +6,33 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = "1.11.3"
     }
+    #     kubernetes = {
+    #   source = "hashicorp/kubernetes"
+    #   version = "2.4.1"
+    # }
   }
 }
 
 provider "kubectl" {
   # Configuration options
   alias                  = "gke-1"
-  host                   = "https://34.134.252.150"
+  host                   = "https://35.235.87.26"
   cluster_ca_certificate = base64decode("./gke.cert")
   token                  = data.google_client_config.default.access_token
 }
-
+# provider "kubernetes" {
+#   # Configuration options
+#     alias                  = "gke-1"
+#   host                   = "https://35.235.87.26"
+#   cluster_ca_certificate = base64decode("./gke.cert")
+#   token                  = data.google_client_config.default.access_token
+# }
+provider "helm" {
+  alias = "gke-1"
+  kubernetes {
+    host                   = "https://35.235.87.26"
+  cluster_ca_certificate = base64decode("./gke.cert")
+  token                  = data.google_client_config.default.access_token
+  config_path = "~/.kube/config"
+  }
+}
